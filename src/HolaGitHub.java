27fa5939bcf5429348752b4e66d4b294ed517598
carlosjper <82869482+carlosjper@users.noreply.github.com>
2021-04-21 11:12:18 +0200

    Update HolaGitHub.java
    Hice cambios. Quité la s que sobraba.

--- src/HolaGitHub.java
+++ src/HolaGitHub.java
@@ -4,7 +4,7 @@ public class HolaGitHub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.outs.println("Hola GitHub es una prueba");
+		System.out.println("Hola GitHub es una prueba");
 		
 		
 	}
